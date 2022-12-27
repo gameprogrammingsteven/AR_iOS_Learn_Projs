575d8e67091bc0ff53f9c0c2c6892d23ef9cb263
StephenJohnson <sjjair@yahoo.com>
2022-12-26 18:36:51 -0800

    Erase boiler code, initial commit

---
+++ GestureARKitLearningProjects/GestureARKitLearningProjects/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  GestureARKitLearningProjects
+//
+//  Created by Stephen Johnson on 12/26/22.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
