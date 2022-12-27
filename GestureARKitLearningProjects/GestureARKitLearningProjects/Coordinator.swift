fb7fa87c4ecc309827f2b79a9d9d51e8d6876d95
StephenJohnson <sjjair@yahoo.com>
2022-12-26 20:31:58 -0800

    Add meshes via gesture

--- GestureARKitLearningProjects/GestureARKitLearningProjects/Coordinator.swift
+++ GestureARKitLearningProjects/GestureARKitLearningProjects/Coordinator.swift
@@ -22,5 +22,20 @@ class Coordinator: NSObject, ARSessionDelegate {
         if let tappedEntity = view.entity(at: screenLocation) as? ModelEntity {
             tappedEntity.model?.materials = [SimpleMaterial.randomColoredMaterial()]
         }
+        
+        let hits = view.raycast(from: screenLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstCollision = hits.first {
+            let planeAnchor = ARAnchor(name: "New Anchor From Ray", transform: firstCollision.worldTransform)
+            view.session.add(anchor: planeAnchor)
+            
+            let aSimpleMat = SimpleMaterial(color: .red, isMetallic: true) //Can work with lights in room.
+            let addedMesh = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [aSimpleMat])
+
+            let anchorEntity = AnchorEntity(.plane(.horizontal, classification: .table, minimumBounds: SIMD2(x: 0.2, y: 0.2)))
+            anchorEntity.addChild(addedMesh)
+            
+            view.scene.addAnchor(anchorEntity)
+        }
     }
 }
