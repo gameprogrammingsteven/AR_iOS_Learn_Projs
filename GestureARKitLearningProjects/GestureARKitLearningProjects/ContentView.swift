1aa1b9a69c300db3c05d923ca25d442cfcfbf8d0
StephenJohnson <sjjair@yahoo.com>
2022-12-26 19:28:06 -0800

    Add cube, prep for input pattern w/ coordinator

--- GestureARKitLearningProjects/GestureARKitLearningProjects/ContentView.swift
+++ GestureARKitLearningProjects/GestureARKitLearningProjects/ContentView.swift
@@ -20,6 +20,15 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(tapScreen:)))
+        var horizontalAnchor = AnchorEntity(.plane(.horizontal, classification: .floor, minimumBounds:  SIMD2(x: 0.2, y: 0.2))) //20cm minimum bound
+        horizontalAnchor.name = "First Plane"
+        
+        let aSimpleMat = SimpleMaterial(color: .blue, isMetallic: false) //Can work with lights in room.
+        let cube = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [aSimpleMat])
+        arView.scene.anchors.append(horizontalAnchor)
+        
+        
         return arView
         
     }
---
+++ GestureARKitLearningProjects/GestureARKitLearningProjects/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  GestureARKitLearningProjects
+//
+//  Created by Stephen Johnson on 12/26/22.
+//
+
+import Foundation
+import RealityKit
+import ARKit
+
+class Coordinator: NSObject, ARSessionDelegate {
+    
+    @objc func tapScreen(_ recognizer: UITapGestureRecognizer){
+        
+    }
+}
