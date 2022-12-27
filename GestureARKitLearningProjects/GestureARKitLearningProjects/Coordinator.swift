7a125dddf6421f9ceba45c78985af456a41ee181
StephenJohnson <sjjair@yahoo.com>
2022-12-26 20:56:33 -0800

    Added: USDZ models

--- GestureARKitLearningProjects/GestureARKitLearningProjects/Coordinator.swift
+++ GestureARKitLearningProjects/GestureARKitLearningProjects/Coordinator.swift
@@ -8,10 +8,12 @@
 import Foundation
 import RealityKit
 import ARKit
+import Combine
 
 class Coordinator: NSObject { //Removed ARKit stuff in favor of Reality
     
     weak var view: ARView?
+    var cancellable: AnyCancellable?
     
     @objc func tapScreen(_ recognizer: UITapGestureRecognizer){
         guard let view = self.view else {
@@ -29,14 +31,36 @@ class Coordinator: NSObject { //Removed ARKit stuff in favor of Reality
 //            view.session.add(anchor: planeAnchor) No more session
             
             let anchorEntity = AnchorEntity(world: firstCollision.worldTransform)
-            let aSimpleMat = SimpleMaterial(color: .red, isMetallic: true) //Can work with lights in room.
-            let addedMesh = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [aSimpleMat])
-            addedMesh.generateCollisionShapes(recursive: true)
+            
+            
+            cancellable = ModelEntity.loadAsync(named: "fenderGuitar")
+                .sink { loadComplete in
+                    if case let .failure(e) = loadComplete {
+                        print("Unable to load Guitar")
+                        self.cancellable?.cancel()
+                    }
+                } receiveValue: { entity in
+                    anchorEntity.addChild(entity)
+                }
 
-            anchorEntity.addChild(addedMesh)
+            //Slower code, erase/comment next line
+            guard let guitar = try? ModelEntity.load(named: "fenderGuitar") else {
+                print("fender guitar not found") //Debug print, not in prod.
+                return
+            }
+            
+            anchorEntity.addChild(guitar)
+            
+//            let aSimpleMat = SimpleMaterial(color: .red, isMetallic: true) //Can work with lights in room.
+//            let addedMesh = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [aSimpleMat])
+//            addedMesh.generateCollisionShapes(recursive: true)
+//
+//            anchorEntity.addChild(addedMesh)
+            
+            
             view.scene.addAnchor(anchorEntity)
             
-            view.installGestures(.all, for: addedMesh)
+            view.installGestures(.all, for: guitar)
         }
     }
 }
